e939711c0d0db1821b2199c4e4bd79b16f5aac88
Ali Eren Konak <alieren.konak@gmail.com>
2025-01-02 23:06:57 +0300

    updated telegram notification

--- stok.py
+++ stok.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 import asyncio
 from telegram import Bot
 
 # Telegram bot token ve chat ID'leri
-TELEGRAM_BOT_TOKEN = "" # token 
+TELEGRAM_BOT_TOKEN = ""
 CHAT_IDS = [""] 
 
 # URL ve kontrol edilecek beden
@@ -14,12 +15,13 @@ SIZE_TO_CHECK = "S"
 # Telegram bot ile mesaj gönderme fonksiyonu
 async def send_telegram_notification(message):
     bot = Bot(token=TELEGRAM_BOT_TOKEN)
-    for chat_id in CHAT_IDS: 
+    for chat_id in CHAT_IDS:
         await bot.send_message(chat_id=chat_id, text=message)
 
 # Stok kontrol fonksiyonu
 def check_stock():
     try:
+        
         response = requests.get(PRODUCT_URL, headers={"User-Agent": "Mozilla/5.0"})
         response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
@@ -28,13 +30,13 @@ def check_stock():
         if size_element:
             size_label = size_element.find("div", class_="size-selector-sizes-size__label", string=SIZE_TO_CHECK)
             if size_label and "disabled" not in size_element.get("class", []):
-                return True  # beden stokta
-        return False  # beden stokta değil
+                return True  # Beden stokta
+        return False  # Beden stokta değil
 
     except Exception as e:
         print(f"Hata oluştu: {e}")
         return False
-    
+
 async def main():
     print("Stok kontrol sistemi başlatıldı...")
     while True:
@@ -42,8 +44,9 @@ async def main():
             await send_telegram_notification(f"{SIZE_TO_CHECK} bedeni stokta! Hemen al: {PRODUCT_URL}")
             break 
         else:
-            print(f"{SIZE_TO_CHECK} bedeni stokta değil. Tekrar kontrol ediliyor...")
-        await asyncio.sleep(300) 
+            await send_telegram_notification(f"{SIZE_TO_CHECK} bedeni stokta değil. Tekrar kontrol ediliyor...")
+            print("Stok yok, tekrar kontrol ediliyor...")
+        await asyncio.sleep(300)
 
 if __name__ == "__main__":
     asyncio.run(main())
